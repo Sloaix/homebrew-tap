dc1c1f480ecae03fba9f9fe7a1dc1d55183ff7f3
xiaolishang <faith.epiphone@qq.com>
2018-04-24 00:30:16 +0800

    modify sha256

--- duershowcli.rb
+++ duershowcli.rb
@@ -3,7 +3,7 @@ class Duershowcli < Formula
   homepage ""
   version "0.0.2"
   url "https://github.com/lsxiao/homebrew-tap/blob/master/duershowcli-0.0.2.zip?raw=true"
-  sha256 "71fe3244ea29b203da48f99c75641416d9edc039d47a1744a3af5d1a13504925"
+  sha256 "8d05fd5043c976960d235ca38899bcfed2edbdca1d75ddb8b3beefb7d28225dd"
 
   def install
     bin.install "duer"
