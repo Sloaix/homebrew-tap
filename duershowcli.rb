3281ca1ab32801015db767e2aa6868993ad8351b
xiaolishang <faith.epiphone@qq.com>
2018-04-23 17:18:47 +0800

    add release package

---
+++ duershowcli.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Duershowcli < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/lsxiao/homebrew-tap/blob/master/duershowcli-0.0.1.zip?raw=true"
+  sha256 "1d835acb0bc65e0a5f0610fab8b1a681f0daf1ef452bcf3c32ea59cba78f4e99"
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "duer"
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    #system "./configure", "--disable-debug",
+    #                      "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    #system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test duershowcli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
