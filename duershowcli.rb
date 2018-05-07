77ca03e3fdc159781270322aa73dfedf38f76a7d
Lison <faith.epiphone@qq.com>
2018-05-07 20:58:50 +0800

    Update duershowcli.rb

--- duershowcli.rb
+++ duershowcli.rb
@@ -1,7 +1,7 @@
 class Duershowcli < Formula
   desc "duershow cli tool"
   homepage ""
-  version "0.0.2"
+  version "0.0.3"
   url "https://github.com/lsxiao/homebrew-tap/blob/master/duershowcli-0.0.3.zip?raw=true"
   sha256 "207524eae1037839b5121218e9dcc7ba3dca2b072e9fae41cc2565802913e0b1"
 
