c6b8b49dd5b9e43e90bb07db4424c2b6ff06a0c8
xiaolishang <faith.epiphone@qq.com>
2018-09-27 17:13:25 +0800

    [Changed]更新0.0.4版本,duer bc 和duer say 功能对齐

--- duershowcli.rb
+++ duershowcli.rb
@@ -1,9 +1,9 @@
 class Duershowcli < Formula
   desc "duershow cli tool"
   homepage ""
-  version "0.0.3"
-  url "https://github.com/lsxiao/homebrew-tap/blob/master/duershowcli-0.0.3.zip?raw=true"
-  sha256 "26f4f345d1d2a7d027f92ac3b6eb5b30cd9f7a6b4a298da62ad227061f2740fa"
+  version "0.0.4"
+  url "https://github.com/lsxiao/homebrew-tap/blob/master/duershowcli-0.0.4.zip?raw=true"
+  sha256 "638d69ce387a5ba581e0e88b883f38af52b5db5d51bada1791dbcdc1636d7169"
 
   def install
     bin.install "duer"
