0aeebf1ba0339dc6af79028f34f4a9909c293126
xiaolishang <faith.epiphone@qq.com>
2018-10-03 19:15:28 +0800

    [Changed]新增dump命令

--- duershowcli.rb
+++ duershowcli.rb
@@ -1,9 +1,9 @@
 class Duershowcli < Formula
   desc "duershow cli tool"
   homepage ""
-  version "0.0.4"
-  url "https://github.com/lsxiao/homebrew-tap/blob/master/duershowcli-0.0.4.zip?raw=true"
-  sha256 "638d69ce387a5ba581e0e88b883f38af52b5db5d51bada1791dbcdc1636d7169"
+  version "0.0.5"
+  url "https://github.com/lsxiao/homebrew-tap/blob/master/duershowcli-0.0.5.zip?raw=true"
+  sha256 "419b763a2b5f389fd0b43ffd679aad34faf1b0488b4f169bb830ba2db0663c84"
 
   def install
     bin.install "duer"
