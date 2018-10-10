2747601e3e084a88a9680fbaa1e2c1423764f448
xiaolishang <faith.epiphone@qq.com>
2018-10-10 13:28:20 +0800

    [Changed] build DuerShowCli 2.7.1

--- duershowcli.rb
+++ duershowcli.rb
@@ -1,9 +1,9 @@
 class Duershowcli < Formula
   desc "duershow cli tool"
   homepage ""
-  version "0.0.5"
-  url "https://github.com/lsxiao/homebrew-tap/blob/master/duershowcli-0.0.5.zip?raw=true"
-  sha256 "b890645e399326b637763b65416fb5c4d2ee05bf36831da5698bc828fa7a96e8"
+  version "0.0.6"
+  url "https://github.com/lsxiao/homebrew-tap/blob/master/duershowcli-0.0.6.zip?raw=true"
+  sha256 "38646bcb5dc1410dcaf4157d09a3cc59cb47ff8ebc64513bce1e96b47ab22952"
 
   def install
     bin.install "duer"
